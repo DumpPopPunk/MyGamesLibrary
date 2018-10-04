96ea0e0ac3fcf9c184abe2d09b2c6b86c2703c48
Samuel Medina <circasamm@gmail.com>
2018-10-04 01:45:24 -0500

    Everything is created. Center some stuff and give a background

---
+++ .idea/Project/profile/userProfile.js
@@ -0,0 +1,15 @@
+$(function() { 
+			$("#one").addClass("progress-bar-black");
+			});
+			
+$(function() { 
+			$("#two").addClass("progress-bar-green");
+			});
+			
+$(function() { 
+			$("#three").addClass("progress-bar-blue");
+			});
+			
+$(function() { 
+			$("#four").addClass("progress-bar-red");
+			});
